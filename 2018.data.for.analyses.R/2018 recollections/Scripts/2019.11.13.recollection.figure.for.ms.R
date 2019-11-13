acbb19e8118a49488571c42a13074301c2456620
George C Jarvis <george.jarvis.235@my.csun.edu>
2019-11-13 19:38:57 +1100

    finalized script for figure for recollections recollections

--- 2018.data.for.analyses.R/2018 recollections/Scripts/2019.11.13.recollection.figure.for.ms.R
+++ 2018.data.for.analyses.R/2018 recollections/Scripts/2019.11.13.recollection.figure.for.ms.R
@@ -21,19 +21,27 @@ reco.means$Treatment<-ordered(reco.means$Treatment,levels=c("Low","Medium","High
 
 #start here when you get back
 png("Output/2019.11.13.bw.9.5x5.5.300dpi.png", width = 9.5, height = 5.5, units = 'in', res = 300)
+png("Output/2019.11.13.bw.7.5x5.5.300dpi.png", width = 7.5, height = 5.5, units = 'in', res = 300)
+png("Output/2019.11.13.bw.6.5x5.5.300dpi.png", width = 6.5, height = 5.5, units = 'in', res = 300)
 
 reco.plot<- ggplot(reco.means, aes(x=Treatment, y=x, fill=Treatment)) +
-  geom_bar(stat="identity", colour= "black", width = 0.85, position="dodge")+ 
+  geom_bar(stat="identity", colour= "black", width = 0.5, position="dodge")+ 
   scale_x_discrete(limits=c("Low","Medium","High"))+
-  theme_classic() + theme(legend.position="none") + #scale_fill_discrete(limits=c("Low","Medium", "High")) +
-  #theme(legend.key.size = unit(1.3,'line')) + 
-  #theme(legend.title=element_text(size=34) , legend.text=element_text(size=20)) 
-  scale_fill_manual(values=c("#0072B2","#009E73","#D55E00")) + 
-  theme(axis.text.x=element_text(size=32, colour="black"),axis.text.y=element_text(size=32, colour="black"), axis.title=element_text(size=37,face="bold")) +
-  theme(axis.title.y = element_text(size= 37, margin = margin(t = 0, r = 20, b = 0, l = 0)), axis.title.x = element_text(margin = margin(t = 25, r = 0, b = 0, l = 0)), axis.text.x = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0))) +
-  theme(axis.ticks.x = element_blank()) + scale_y_continuous(expand = c(0, 0),limits = c(0,0.45))
+  theme_classic() + 
+  labs(x="Risk Treatment", y="Survival 
+  (gobies recollected per reef)") +
+  theme(legend.position="none") + 
+  scale_fill_manual(values=c("grey", "grey", "grey")) +
+  theme(axis.text.x=element_text(size=20, colour="black"),
+        axis.text.y=element_text(size=20, colour="black"), 
+        axis.title=element_text(size=20))+
+  theme(axis.title.y = element_text(margin = margin(t = 0, r = 20, b = 0, l = 0)), 
+        axis.title.x = element_text(margin = margin(t = 12, r = 0, b = 0, l = 0)), 
+        axis.text.x = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0))) +
+  theme(legend.text=element_text(size=18)) +
+  theme(legend.title =element_text(size=20))+
+  theme(axis.ticks.x = element_blank()) + scale_y_continuous(expand = c(0,0),limits = c(0,6.5))
 reco.plot + geom_linerange(aes(ymin=x-se, ymax=x+se), size=0.5,   
-                           position=position_dodge(.85)) + theme(text = element_text(family="Arial")) +
-  labs(x="Risk Treatment", y="Proportion of Gobies Recollected")
-
+                           position=position_dodge(.85)) + theme(text = element_text(family="Arial"))
+#not sure why it's off the x axis now...has something to do with breaks
 dev.off()
