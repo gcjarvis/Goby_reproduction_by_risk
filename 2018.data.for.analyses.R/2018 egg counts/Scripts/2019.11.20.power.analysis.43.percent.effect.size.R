4b3d604e22c9d4f0e59ed7fddbd8cfed5a12a615
George C Jarvis <george.jarvis.235@my.csun.edu>
2019-11-20 08:06:27 +1100

    did power analysis for egg counts, based on Mukherjee et al. 2014 study, 43% effect size

---
+++ 2018.data.for.analyses.R/2018 egg counts/Scripts/2019.11.20.power.analysis.43.percent.effect.size.R
@@ -0,0 +1,22 @@
+# Description: calculation for power analysis with 43% reduction in output
+# Author: George C Jarvis
+# Date: Wed Nov 20 07:46:50 2019
+# Notes: this effect size was taken from (Mukherjee et al. 2014)
+# --------------
+
+library(pwr)
+
+pwr.f2.test(u = 2, v = 99.09, f2 = 0.43, sig.level = 0.05, power = NULL)
+
+
+
+#parameters:
+
+#u degrees of freedom for numerator
+#v degrees of freedomfor denominator
+#f2 effect size
+#sig.level Significance level (Type I error probability)
+#power Power of test (1 minus Type II error probability)
+
+
+pwr.f2.test(u = NULL, v = NULL, f2 = NULL, sig.level = 0.05, power = NULL)
