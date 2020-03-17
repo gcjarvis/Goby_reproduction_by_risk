7040c6e5da90c9a931a96634e2db05a6a6a24508
George C Jarvis <george.jarvis.235@my.csun.edu>
2020-03-18 10:09:25 +1100

    cleaned power analysis script a tad bit more

--- 2018.data.for.analyses.R/2018 egg counts/Scripts/2020.3.18.power.analysis.R
+++ 2018.data.for.analyses.R/2018 egg counts/Scripts/2020.3.18.power.analysis.R
@@ -12,15 +12,21 @@ library(pwr)
 # - manipulating denDF to represent conservative (denDF = 8) and liberal (denDF > 8) estimates based on model output
 # -- the most liberal denDF for my treatment factor is 99, which we all think is incorrect
 
-#parameters:
+# usage: pwr.f2.test(u = NULL, v = NULL, f2 = NULL, sig.level = 0.05, power = NULL)
+
+# arguments:
 
 # u =  degrees of freedom for numerator
 # v =  degrees of freedom for denominator
 # f2 = effect size
-# sig.level = alpha (probability of Type I error)
-# power = 1 - beta (1 minus probability of Type II error)
+# sig.level = Significance level (Type I error probability)
+# power = Power of test (1 minus Type II error probability)
 
-# solving for power (i.e. power parameter is "NULL")
+#NOTE: the parameter that is left as "NULL" is the one that is solved for
+# - In this output, the percentage value 
+
+# 1. solving for power (i.e. power parameter is "NULL"), with an effect of 40% that was reported in lab study for risk vs. reproduction
+# - Increasing the denDF for each iteration
 
 pwr.f2.test(u = 2, v = 8, f2 = 0.40, sig.level = 0.05, power = NULL) #32%
 
@@ -28,13 +34,13 @@ pwr.f2.test(u = 2, v = 10, f2 = 0.40, sig.level = 0.05, power = NULL) #40%
 
 pwr.f2.test(u = 2, v = 20, f2 = 0.40, sig.level = 0.05, power = NULL) #71%
 
-pwr.f2.test(u = 2, v = 25, f2 = 0.40, sig.level = 0.05, power = NULL) #81% - would have needed ~ 25 replicates to detect an effect of 40%
+pwr.f2.test(u = 2, v = 25, f2 = 0.40, sig.level = 0.05, power = NULL) #81% - would have needed ~ 25 replicates to have reasonable power to detect an effect of 40%
 
 pwr.f2.test(u = 2, v = 30, f2 = 0.40, sig.level = 0.05, power = NULL) #88%
 
 pwr.f2.test(u = 2, v = 40, f2 = 0.40, sig.level = 0.05, power = NULL) #96%
 
-# same analysis, but with effect size found in my paper (~20% between low-risk and medium- + high-risk treatments)
+# 2. same analysis, but with effect size of 20% found in our study (~20% difference between low-risk and medium- + high-risk treatments)
 
 pwr.f2.test(u = 2, v = 8, f2 = 0.20, sig.level = 0.05, power = NULL) #18%
 
@@ -46,10 +52,10 @@ pwr.f2.test(u = 2, v = 30, f2 = 0.20, sig.level = 0.05, power = NULL) #58%
 
 pwr.f2.test(u = 2, v = 40, f2 = 0.20, sig.level = 0.05, power = NULL) #72%
 
-pwr.f2.test(u = 2, v = 50, f2 = 0.20, sig.level = 0.05, power = NULL) #81% #would have needed ~ 50 replicates to detect an effect of 20%
+pwr.f2.test(u = 2, v = 50, f2 = 0.20, sig.level = 0.05, power = NULL) #81% #would have needed ~ 50 replicates to have reasonable power to detect an effect of 20%
 
 pwr.f2.test(u = 2, v = 60, f2 = 0.20, sig.level = 0.05, power = NULL) #88%
 
 pwr.f2.test(u = 2, v = 70, f2 = 0.20, sig.level = 0.05, power = NULL) #93%
 
-pwr.f2.test(u = 2, v = 80, f2 = 0.20, sig.level = 0.05, power = NULL) #96% #starts to plateau at 80 replicates
+pwr.f2.test(u = 2, v = 80, f2 = 0.20, sig.level = 0.05, power = NULL) #96% #starts to plateau at ~80 replicates
